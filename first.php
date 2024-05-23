dd63731e8ae85356ebf6ef84ac965a0f1432001d
Josphat Waweru Thumi <thumijosphat47@gmail.com>
2024-05-23 22:20:42 +0300

    4

--- first.php
+++ first.php
@@ -130,6 +130,60 @@ echo(round(0.49));
 
 
 //The rand() function generates a random number:
+
+echo(rand());
+//To get more control over the random number, you can add the optional min and max parameters
+// to specify the lowest integer and the highest integer to be returned.
+//For example, if you want a random integer between 10 and 100 (inclusive), use rand(10, 100):
+  echo(rand(10,100));
+  //For a complete reference of all math functions, go to our complete PHP Math Reference.
+//The PHP math reference contains description and example of use, for each function.
+
+
+//A constant is an identifier (name) for a simple value. The value cannot be changed during the script.
+//A valid constant name starts with a letter or underscore (no $ sign before the constant name).
+//Note: Unlike variables, constants are automatically global across the entire script.
+//To create a constant, use the define() function.
+//define(name, value, case-insensitive);
+//name: Specifies the name of the constant
+//value: Specifies the value of the constant
+//case-insensitive: Specifies whether the constant name should be case-insensitive. 
+//Default is false. Note: Defining case-insensitive constants was deprecated in PHP 7.3. PHP 8.0 accepts only false, 
+//the value true will produce a warning.
+define("GREETING", "Welcome to W3Schools.com!", true);
+echo greeting;
+//You can also create a constant by using the const keyword.
+const MYCAR = "Volvo";
+echo MYCAR;
+
+//const are always case-sensitive
+//define() has has a case-insensitive option.
+//const cannot be created inside another block scope, like inside a function or inside an if statement.
+//define can be created inside another block scope.
+
+//From PHP7, you can create an Array constant using the define() function.
+define("cars", [
+  "Alfa Romeo",
+  "BMW",
+  "Toyota"
+]);
+echo cars[0];
+
+//PHP has nine predefined constants that change value depending on where they are used, 
+//and therefor they are called "magic constants".
+//These magic constants are written with a double underscore at the start and the end, 
+//except for the ClassName::class constant.
+//__CLASS__	If used inside a class, the class name is returned.	
+//__DIR__	The directory of the file.	
+//__FILE__	The file name including the full path.	
+//__FUNCTION__	If inside a function, the function name is returned.	
+//__LINE__	The current line number.	
+//__METHOD__	If used inside a function that belongs to a class, both class and function name is returned.	
+//__NAMESPACE__	If used inside a namespace, the name of the namespace is returned.	
+//__TRAIT__	If used inside a trait, the trait name is returned.	
+//ClassName::class	Returns the name of the specified class and the name of the namespace, if any.
+
+
 ?> 
 </pre>
 
